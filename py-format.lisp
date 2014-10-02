ee5a616ca8501a9fa6fe19af42b32f1604293b61
unknown <22FOA@CHIX1C2-22FOA.BAIN.com>
2014-10-01 21:02:52 -0500

    initial import

---
+++ py-format.lisp
@@ -0,0 +1,157 @@
+
+(in-package :py-format)
+
+(defmethod py-format-method (obj format-spec)
+  (if (string= format-spec "")
+      (py-str obj)
+      (format nil (if format-spec obj))))
+
+(defmethod py-str (obj)
+  (format nil "~A" obj))
+
+(defmethod py-repr (obj)
+  (format nil "~S" obj))
+
+(defun py-formatter-convert-field (obj conversion)
+  "Conversion must be one of 's' or 'r', equivalent to calling str(obj) or repr(obj)"
+  (if conversion
+      (ecase conversion
+        (#\s (py-str obj))
+        (#\r (py-repr obj)))
+      obj))
+
+(defun py-formatter-parse-field (field-string)
+  "Parses the interior of a format string field (i.e., the part between curly-braces).
+Returns a list (field-name format-spec conversion)"
+  (declare (type string field-string))
+  ; <field>[!conversion][:<field-spec>]
+  ;        ^ field-end    ^ field-spec-start
+  (let* ((conversion nil)
+        (field-len (length field-string))
+        (format-spec-start field-len))
+    (declare (type integer field-len format-spec-start))
+    (if (eql field-len 0) '("" "" nil)
+    (destructuring-bind (field-end c)
+        (loop
+           for i from 1
+           for c across field-string
+           when (or (eql c #\:) (eql c #\!) (= i field-len))
+           return (list i c))
+      (declare (type integer field-end))
+      (declare (type character c))
+      (when (or (eql c #\:) (eql c #\!))
+        (setq format-spec-start field-end)
+        (decf field-end)
+        (when (eql c #\!)
+          (when (>= format-spec-start field-len)
+            (error "End of format while looking for conversion specifier"))
+          (setf conversion (char field-string format-spec-start))
+          (incf format-spec-start)
+          (when (< format-spec-start field-len)
+            (unless (eql (char field-string format-spec-start) #\:)
+              (error "Expected ':' after format specifier"))
+            (incf format-spec-start))))
+      (list (subseq field-string 0 field-end)
+            (subseq field-string format-spec-start field-len)
+                conversion)))))
+
+(defun py-formatter-parse-format-string (format-string)
+  "Generates a list with items of the form (literal-text field-name format-spec conversion).
+Where literal-text is text to print literally, field-name is the reference to the field,
+format-spec is the formatting string to use for the field, and conversion is one of 's' or 'r'"
+  (declare (type string format-string))
+  (loop
+     for base-ptr from 0
+     for markup-follows = nil
+     with fmt-len = (the integer (length format-string))
+     while (< base-ptr fmt-len)
+     collect
+       (let* ((ptr (1+ (loop
+                          for i from base-ptr to (1- fmt-len)
+                          for c = (the character (char format-string i))
+                          when (or (eql c #\{) (eql c #\}))
+                          do (setf markup-follows t)
+                          and return i
+                          maximize i)))
+              (at-end (>= ptr fmt-len))
+              (literal-end ptr)
+              (c (the character (char format-string (1- ptr)))))
+         (when (and (eql c #\}) (or at-end (not (eql c (char format-string ptr)))))
+           (error "Single '}' encountered in format string"))
+         (when (and at-end (eql c #\{))
+           (error "Single '{' encountered in format string"))
+         (if (not at-end)
+             (if (eql c (char format-string ptr))
+                 (progn (incf ptr) (setf markup-follows nil))
+                 (decf literal-end)))
+         (cons (subseq format-string base-ptr literal-end)
+               (if markup-follows
+                   (loop
+                      for i from ptr to (1- fmt-len)
+                      for c = (the character (char format-string i))
+                      with count = 1
+                      when (eql #\{ c) do (incf count)
+                      when (and (eql #\} c) (<= (decf count) 0))
+                      do (setf base-ptr i)
+                      and return (py-formatter-parse-field (or (subseq format-string ptr i) ""))
+                      finally (error "unmatched '{' in format"))
+                   (progn (setf base-ptr literal-end) '(nil nil nil)))))))
+
+(defun py-formatter-field-name-split (field-name)
+  "Given a field name, parse it into getattrs and getitems."
+  (declare (type string field-name))
+  ; abcd.efg[hijk].lmno
+  ; ^    ^   ^     ^
+  (maplist #'(lambda (lst)
+               (let ((first (car lst))
+                     (second (second lst)))
+                 (when (not second) (setf second (cons (1+ (length field-name)) nil)))
+                 (cons (not (cdr first))
+                       (subseq
+                        field-name
+                        (car first)
+                        (- (car second) (if (cdr first) 2 1))))))
+           (cons '(0 . nil)
+                 (loop
+                    for c across field-name
+                    for i from 0
+                    for delim-c = (or (eql c #\[) (eql c #\.))
+                    with in-brackets = nil
+                    when (eql c #\])
+                    do (if in-brackets (setf in-brackets nil) (error "Unmatched ']' in format string"))
+                    when (and in-brackets delim-c)
+                    do (error "Missing ']' in format string")
+                    when delim-c
+                    collect (cons (1+ i) (setf in-brackets (eql c #\[)))))))
+
+(defun py-formatter-get-field (field-name args)
+  "Given a field name, find the object it references"
+  (let* ((fields (py-formatter-field-name-split field-name))
+         (first (car fields))
+         (rest (cdr fields))
+         (obj (nth (parse-integer (cdr first)) args)))
+    (loop
+       for (is-attr . next) in rest
+       do (setf obj (if is-attr (slot-value obj next)
+                        (if (listp obj) (nth (parse-integer next) obj) (gethash next obj)))))
+    obj))
+
+(defun py-vformat (format-string args recursion-depth)
+  (declare (type fixnum recursion-depth))
+  (when (< recursion-depth 0) (error "Max string formatting recursion exceeded"))
+  (apply #'concatenate
+         (cons 'string
+               (loop
+                  for (literal-text field-name format-spec conversion)
+                  in (py-formatter-parse-format-string format-string)
+                  when literal-text collect literal-text
+                  when field-name
+                  collect (py-format-method
+                           (py-formatter-convert-field
+                            (py-formatter-get-field field-name args)
+                            conversion)
+                           (py-vformat format-spec args (the fixnum (1- recursion-depth))))))))
+
+(defun py-format (format-string &rest args)
+  "Formats a python-style format string"
+  (py-vformat format-string args 2))
